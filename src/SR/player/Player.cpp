9d99c4fde92cf497ad7e3e531757f94af532be6d
Iswenzz <alexisnardiello@gmail.com>
2023-05-09 19:06:46 +0200

    Player: FPS Calculation update but not working

--- src/SR/player/Player.cpp
+++ src/SR/player/Player.cpp
@@ -13,6 +13,8 @@ namespace Iswenzz::CoD4x
 
 		this->DemoPlayer = std::make_unique<class DemoPlayer>(this);
 		this->PMove = std::make_unique<class PMove>(this);
+
+		std::fill(this->FrameTimes.begin(), this->FrameTimes.end(), 0);
 	}
 
 	void Player::Spawn()
@@ -22,24 +24,45 @@ namespace Iswenzz::CoD4x
 
 	void Player::CalculateFrame(int time)
 	{
-		if (time > CurrentFrameTime)
-		{
-			PreviousFrameTime = CurrentFrameTime;
-			CurrentFrameTime = time;
-			FrameTimes.push_back(1000 / (CurrentFrameTime - PreviousFrameTime));
-		}
+		FrameTimes[FrameStackIndex++ % PLAYER_FPS_STACK] = time;
+		cl->clFrames++;
 	}
 
 	void Player::CalculateFPS()
 	{
-		if (FrameTimes.empty())
-			return;
+		int i = 0;
+		int maxValue = -1;
+		int value = -1;
 
-		FPS = Utils::VectorAverageMode(FrameTimes);
-		FrameTimes.clear();
-
-		cl->clFPS = FPS;
+		int possibleValues[1000] = { 0 };
+		for (i = 0; i < PLAYER_FPS_STACK; i++)
+		{
+			if (FrameTimes[i] <= 0 || FrameTimes[i] >= 1000)
+				continue;
+			possibleValues[FrameTimes[i]]++;
+		}
+		for (i = 0; i < 16; i++)
+		{
+			if (possibleValues[i] == 0 || possibleValues[i] < maxValue)
+				continue;
+			maxValue = possibleValues[i];
+			value = i;
+		}
+		if (maxValue < 10)
+		{
+			for (i = 16; i < 1000; i++)
+			{
+				if (possibleValues[i] == 0 || possibleValues[i] < maxValue)
+					continue;
+				maxValue = possibleValues[i];
+				value = i;
+			}
+		}
+		cl->clFPS = value > 0 ? static_cast<int>(1000 / value) : 0;
 		cl->clFrames = 0;
+		FrameStackIndex = 0;
+
+		Log::WriteLine("[FPS]: %d %d", value, cl->clFPS);
 	}
 
 	void Player::Packet(msg_t *msg)
@@ -123,6 +146,8 @@ C_EXTERN
 		if (!IsDefinedClient(cl))
 			return;
 
-		SR->Players[cl->gentity->client->ps.clientNum]->CalculateFrame(cl->gentity->client->ps.commandTime);
+		int time = cmd->serverTime - cl->lastUsercmd.serverTime;
+		SR->Players[cl->gentity->client->ps.clientNum]->CalculateFrame(time);
+		// Log::WriteLine("[Frame] %d %d %d", cmd->serverTime, cl->lastUsercmd.serverTime, time);
 	}
 }
