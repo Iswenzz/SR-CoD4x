d6577a62b30908156a138eef6359d8be2dfd5e12
Iswenzz <alexisnardiello@gmail.com>
2023-09-19 19:53:58 +0200

    Scripts: Install & Compile

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# CGSC
+echo -e "[+] CGSC"
+cmake --build src/CGSC/build
+
+# SR
+echo -e "[+] SR"
+cmake --build src/SR/build
+
+# CoD4x
+echo -e "[+] CoD4x"
+make
+
+# gsclib
+echo -e "[+] gsclib"
+cmake --build plugins/gsclib/build
+
+# Binary
+echo -e "[+] Binary"
+cp -v bin/cod4x18_dedrun /home/cod4/cod4x18_dedrun && chmod u+x /home/cod4/cod4x18_dedrun
