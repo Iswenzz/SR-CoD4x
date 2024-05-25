0ab3c559a68124e319b5482842900fbb767125c9
Iswenzz <alexisnardiello@gmail.com>
2024-05-26 00:43:52 +0200

    API: C/C++ header

--- src/SR/src/API.cpp
+++ src/SR/src/API.cpp
@@ -1,169 +1,171 @@
 #include "API.hpp"
+
 #include "Application.hpp"
 
-IZ_C
+IZ_C_START
+
+void SR_Initialize()
 {
-	void SR_Initialize()
-	{
-		SR = new Application();
-	}
-
-	void SR_Shutdown()
-	{
-		delete SR;
-	}
-
-	void SR_InitializePlayer(client_t * cl)
-	{
-		if (!IsDefinedClient(cl))
-			return;
-
-		Player::Add(cl);
-	}
-
-	void SR_FreePlayer(client_t * cl)
-	{
-		if (!IsDefinedClient(cl))
-			return;
-
-		auto player = Player::Get(cl->gentity->s.number);
-		if (player)
-			player->Disconnect();
-	}
-
-	void SR_ClientSpawn(gclient_t * client)
-	{
-		if (!IsDefinedGClient(client))
-			return;
-
-		Player::Get(client->ps.clientNum)->Spawn();
-	}
-
-	void SR_CalculateFrame(client_t * cl, usercmd_t * cmd)
-	{
-		if (!IsDefinedClient(cl))
-			return;
-
-		int time = cmd->serverTime - cl->lastUsercmd.serverTime;
-		Player::Get(cl->gentity->s.number)->CalculateFrame(time);
-	}
-
-	void SR_InitializeEntity(gentity_t * ent)
-	{
-		if (!IsDefinedEntity(ent))
-			return;
-
-		Entity::Add(ent);
-	}
-
-	void SR_SetMapAmbient(const char *alias, int volume)
-	{
-		SR->Server->Map->SetAmbient(alias, volume);
-	}
-
-	void SR_Frame()
-	{
-		SR->Server->Frame();
-	}
-
-	void SR_SpawnServer(const char *levelname)
-	{
-		SR->Server->Spawn(levelname);
-	}
-
-	void SR_Restart()
-	{
-		SR->Server->Restart();
-	}
-
-	void SR_BroadcastVoice(gentity_t * talker, VoicePacket_t * packet)
-	{
-		SR->Server->Voice->BroadcastVoice(talker, packet);
-	}
-
-	qboolean SR_DemoIsPlaying(client_t * cl)
-	{
-		if (!IsDefinedClient(cl))
-			return qfalse;
-
-		auto player = Player::Get(cl->gentity->client->ps.clientNum);
-		return static_cast<qboolean>(player && !!player->DemoPlayer->Demo);
-	}
-
-	void SR_DemoUpdateEntity(client_t * cl, snapshotInfo_t * snapInfo, msg_t * msg, const int time, entityState_t *from,
-		entityState_t *to, qboolean force)
-	{
-		auto player = Player::Get(cl->gentity->client->ps.clientNum);
-		player->DemoPlayer->UpdateEntity(snapInfo, msg, time, from, to, force);
-	}
-
-	void SR_DemoButton(client_t * cl, usercmd_t * cmd)
-	{
-		if (!IsDefinedClient(cl))
-			return;
-
-		cl->gentity->client->ps.dofNearStart = *reinterpret_cast<float *>(&cmd->forwardmove);
-		cl->gentity->client->ps.dofNearEnd = *reinterpret_cast<float *>(&cmd->rightmove);
-		cl->gentity->client->ps.dofFarStart = *reinterpret_cast<float *>(&cmd->buttons);
-	}
-
-	void SR_DemoFrame(client_t * cl)
-	{
-		DemoRecord::Frame(cl);
-	}
-
-	void SR_Packet(netadr_t * from, client_t * cl, msg_t * msg)
-	{
-		SR->Netchan->Packet(from, cl, msg);
-	}
-
-	void SR_Print(conChannel_t channel, char *msg)
-	{
-		Log::Write(static_cast<Channel>(channel), msg);
-	}
-
-	OPTIMIZE3 int SR_PmoveGetSpeed(playerState_t * ps)
-	{
-		return Player::Get(ps->clientNum)->PMove->GetSpeed();
-	}
-
-	float SR_PmoveGetSpeedScale(playerState_t * ps)
-	{
-		return Player::Get(ps->clientNum)->PMove->GetSpeedScale();
-	}
-
-	OPTIMIZE3 int SR_PmoveGetGravity(playerState_t * ps)
-	{
-		return Player::Get(ps->clientNum)->PMove->GetGravity();
-	}
-
-	float SR_PmoveGetJumpHeight(unsigned int num)
-	{
-		return Player::Get(num)->PMove->GetJumpHeight();
-	}
-
-	void SR_JumpUpdateSurface(playerState_s * ps, pml_t * pml)
-	{
-		Player::Get(ps->clientNum)->PMove->JumpUpdateSurface(pml);
-	}
-
-	void SR_NetchanDebugSize(int size)
-	{
-		Debug::NetchanPacketSize(size);
-	}
-
-	qboolean SR_VegasConnect(netadr_t * from, msg_t * msg, int *connectionId)
-	{
-		return static_cast<qboolean>(SR->Server->Vegas->Connect(from, msg));
-	}
-
-	void SR_VegasDisconnect(netadr_t * from, int connectionId)
-	{
-		SR->Server->Vegas->Disconnect(from);
-	}
-
-	int SR_VegasMessage(netadr_t * from, msg_t * msg, int connectionId)
-	{
-		return SR->Server->Vegas->Message(from, msg);
-	}
+	SR = new Application();
 }
+
+void SR_Shutdown()
+{
+	delete SR;
+}
+
+void SR_InitializePlayer(client_t *cl)
+{
+	if (!IsDefinedClient(cl))
+		return;
+
+	Player::Add(cl);
+}
+
+void SR_FreePlayer(client_t *cl)
+{
+	if (!IsDefinedClient(cl))
+		return;
+
+	auto player = Player::Get(cl->gentity->s.number);
+	if (player)
+		player->Disconnect();
+}
+
+void SR_ClientSpawn(gclient_t *client)
+{
+	if (!IsDefinedGClient(client))
+		return;
+
+	Player::Get(client->ps.clientNum)->Spawn();
+}
+
+void SR_CalculateFrame(client_t *cl, usercmd_t *cmd)
+{
+	if (!IsDefinedClient(cl))
+		return;
+
+	int time = cmd->serverTime - cl->lastUsercmd.serverTime;
+	Player::Get(cl->gentity->s.number)->CalculateFrame(time);
+}
+
+void SR_InitializeEntity(gentity_t *ent)
+{
+	if (!IsDefinedEntity(ent))
+		return;
+
+	Entity::Add(ent);
+}
+
+void SR_SetMapAmbient(const char *alias, int volume)
+{
+	SR->Server->Map->SetAmbient(alias, volume);
+}
+
+void SR_Frame()
+{
+	SR->Server->Frame();
+}
+
+void SR_SpawnServer(const char *levelname)
+{
+	SR->Server->Spawn(levelname);
+}
+
+void SR_Restart()
+{
+	SR->Server->Restart();
+}
+
+void SR_BroadcastVoice(gentity_t *talker, VoicePacket_t *packet)
+{
+	SR->Server->Voice->BroadcastVoice(talker, packet);
+}
+
+qboolean SR_DemoIsPlaying(client_t *cl)
+{
+	if (!IsDefinedClient(cl))
+		return qfalse;
+
+	auto player = Player::Get(cl->gentity->client->ps.clientNum);
+	return static_cast<qboolean>(player && !!player->DemoPlayer->Demo);
+}
+
+void SR_DemoUpdateEntity(client_t *cl, snapshotInfo_t *snapInfo, msg_t *msg, const int time, entityState_t *from,
+	entityState_t *to, qboolean force)
+{
+	auto player = Player::Get(cl->gentity->client->ps.clientNum);
+	player->DemoPlayer->UpdateEntity(snapInfo, msg, time, from, to, force);
+}
+
+void SR_DemoButton(client_t *cl, usercmd_t *cmd)
+{
+	if (!IsDefinedClient(cl))
+		return;
+
+	cl->gentity->client->ps.dofNearStart = *reinterpret_cast<float *>(&cmd->forwardmove);
+	cl->gentity->client->ps.dofNearEnd = *reinterpret_cast<float *>(&cmd->rightmove);
+	cl->gentity->client->ps.dofFarStart = *reinterpret_cast<float *>(&cmd->buttons);
+}
+
+void SR_DemoFrame(client_t *cl)
+{
+	DemoRecord::Frame(cl);
+}
+
+void SR_Packet(netadr_t *from, client_t *cl, msg_t *msg)
+{
+	SR->Netchan->Packet(from, cl, msg);
+}
+
+void SR_Print(conChannel_t channel, char *msg)
+{
+	Log::Write(static_cast<Channel>(channel), msg);
+}
+
+int SR_PmoveGetSpeed(playerState_t *ps)
+{
+	return Player::Get(ps->clientNum)->PMove->GetSpeed();
+}
+
+float SR_PmoveGetSpeedScale(playerState_t *ps)
+{
+	return Player::Get(ps->clientNum)->PMove->GetSpeedScale();
+}
+
+int SR_PmoveGetGravity(playerState_t *ps)
+{
+	return Player::Get(ps->clientNum)->PMove->GetGravity();
+}
+
+float SR_PmoveGetJumpHeight(unsigned int num)
+{
+	return Player::Get(num)->PMove->GetJumpHeight();
+}
+
+void SR_JumpUpdateSurface(playerState_s *ps, pml_t *pml)
+{
+	Player::Get(ps->clientNum)->PMove->JumpUpdateSurface(pml);
+}
+
+void SR_NetchanDebugSize(int size)
+{
+	Debug::NetchanPacketSize(size);
+}
+
+qboolean SR_VegasConnect(netadr_t *from, msg_t *msg, int *connectionId)
+{
+	return static_cast<qboolean>(SR->Server->Vegas->Connect(from, msg));
+}
+
+void SR_VegasDisconnect(netadr_t *from, int connectionId)
+{
+	SR->Server->Vegas->Disconnect(from);
+}
+
+int SR_VegasMessage(netadr_t *from, msg_t *msg, int connectionId)
+{
+	return SR->Server->Vegas->Message(from, msg);
+}
+
+IZ_C_END
--- src/SR/src/API.hpp
+++ src/SR/src/API.hpp
@@ -1,44 +1,43 @@
 #pragma once
 #include "Macros/Base.hpp"
 
-IZ_C
-{
-	// clang-format off
-	#include <bg_public.h>
-	#include <server.h>
-	// clang-format on
+#include <bg_public.h>
+#include <server.h>
 
-	void SR_Initialize();
-	void SR_Shutdown();
-	void SR_SpawnServer(const char *levelname);
-	void SR_Restart();
-	void SR_Frame();
-	void SR_Packet(netadr_t * from, client_t * cl, msg_t * msg);
+IZ_C_START
 
-	void SR_InitializePlayer(client_t * cl);
-	void SR_FreePlayer(client_t * cl);
-	void SR_InitializeEntity(gentity_t * ent);
-	void SR_ClientSpawn(gclient_t * client);
-	void SR_CalculateFrame(client_t * cl, usercmd_t * cmd);
-	void SR_BroadcastVoice(gentity_t * talker, struct VoicePacket_t * packet);
+void SR_Initialize();
+void SR_Shutdown();
+void SR_SpawnServer(const char *levelname);
+void SR_Restart();
+void SR_Frame();
+void SR_Packet(netadr_t *from, client_t *cl, msg_t *msg);
 
-	void SR_NetchanDebugSize(int size);
-	void SR_SetMapAmbient(const char *alias, int volume);
-	void SR_Print(conChannel_t channel, char *msg);
+void SR_InitializePlayer(client_t *cl);
+void SR_FreePlayer(client_t *cl);
+void SR_InitializeEntity(gentity_t *ent);
+void SR_ClientSpawn(gclient_t *client);
+void SR_CalculateFrame(client_t *cl, usercmd_t *cmd);
+void SR_BroadcastVoice(gentity_t *talker, struct VoicePacket_t *packet);
 
-	void SR_DemoFrame(client_t * cl);
-	qboolean SR_DemoIsPlaying(client_t * cl);
-	void SR_DemoButton(client_t * cl, usercmd_t * cmd);
-	void SR_DemoUpdateEntity(client_t * cl, snapshotInfo_t * snapInfo, msg_t * msg, const int time, entityState_t *from,
-		entityState_t *to, qboolean force);
+void SR_NetchanDebugSize(int size);
+void SR_SetMapAmbient(const char *alias, int volume);
+void SR_Print(conChannel_t channel, char *msg);
 
-	OPTIMIZE3 int SR_PmoveGetSpeed(playerState_t * ps);
-	float SR_PmoveGetSpeedScale(playerState_t * ps);
-	OPTIMIZE3 int SR_PmoveGetGravity(playerState_t * ps);
-	float SR_PmoveGetJumpHeight(unsigned int num);
-	void SR_JumpUpdateSurface(playerState_t * ps, struct pml_t * pml);
+void SR_DemoFrame(client_t *cl);
+qboolean SR_DemoIsPlaying(client_t *cl);
+void SR_DemoButton(client_t *cl, usercmd_t *cmd);
+void SR_DemoUpdateEntity(client_t *cl, snapshotInfo_t *snapInfo, msg_t *msg, const int time, entityState_t *from,
+	entityState_t *to, qboolean force);
 
-	qboolean SR_VegasConnect(netadr_t * from, msg_t * msg, int *connectionId);
-	void SR_VegasDisconnect(netadr_t * from, int connectionId);
-	int SR_VegasMessage(netadr_t * from, msg_t * msg, int connectionId);
-}
+int SR_PmoveGetSpeed(playerState_t *ps);
+float SR_PmoveGetSpeedScale(playerState_t *ps);
+int SR_PmoveGetGravity(playerState_t *ps);
+float SR_PmoveGetJumpHeight(unsigned int num);
+void SR_JumpUpdateSurface(playerState_t *ps, struct pml_t *pml);
+
+qboolean SR_VegasConnect(netadr_t *from, msg_t *msg, int *connectionId);
+void SR_VegasDisconnect(netadr_t *from, int connectionId);
+int SR_VegasMessage(netadr_t *from, msg_t *msg, int connectionId);
+
+IZ_C_END
